4ca0e716a595fd48a3d3d54e587afce2599432bc
920985146 <920985146@qq.com>
2020-08-27 11:30:28 +0800

    项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App hahafa"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import jsonp from 'jsonp'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return {
+      
+    }
+  },
+  mounted(){
+    
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
